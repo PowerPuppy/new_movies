d71ec47793e6b0f426aaf56174999c7c4c6864a9
Chris Cheong <chrisxcheong@gmail.com>
2019-09-16 02:58:14 +0000

    more fake data

--- lib/new_movies/cli.rb
+++ lib/new_movies/cli.rb
@@ -1,9 +1,9 @@
 class NewMovies::CLI
   def call
-    puts "Welcome movie fan!"
-    puts "Please select a movie you would like to find out more about."
+    puts "\nWelcome movie fan!\n"
     get_movies
-    get_user_selection
+    list_movies
+    menu
 
   end
 
@@ -19,11 +19,27 @@ class NewMovies::CLI
         "The Addams Family (2019)"]
   end
 
-  def get_user_selection
+  def list_movies
     #list movies
+    puts "Please select a movie you would like to find out more about."
     @movies.each_with_index do |movie, index|
         puts "#{index+1}. #{movie}"
       end
+  end
 
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the movie you'd like more info on or type exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Info on 1"
+      when "2"
+        puts "info on 2"
+      when "list"
+        list_movies
+      end
+    end
   end
 end
