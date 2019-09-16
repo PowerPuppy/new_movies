13ea2654f6eae465e9f77a6a67f0f89210b62d29
Chris Cheong <chrisxcheong@gmail.com>
2019-09-16 00:49:32 +0000

    fake data to stub out CLI

--- lib/new_movies.rb
+++ lib/new_movies.rb
@@ -2,6 +2,8 @@ require_relative "new_movies/version"
 require_relative "new_movies/cli"
 require_relative "new_movies/scraper"
 require_relative "new_movies/movie"
+require 'pry'
+require 'nokogiri'
 module NewMovies
   class Error < StandardError; end
   # Your code goes here...
--- lib/new_movies/cli.rb
+++ lib/new_movies/cli.rb
@@ -1,6 +1,29 @@
 class NewMovies::CLI
   def call
     puts "Welcome movie fan!"
+    puts "Please select a movie you would like to find out more about."
+    get_movies
+    get_user_selection
+
   end
 
+  def get_movies
+      #to be scraped
+      @movies = ["It Chapter Two (2019)",
+        "Hustlers",
+        "The Goldfinch",
+        "Ad Astra",
+        "Abominable (2019)",
+        "Gemini Man",
+        "Joker (2019)",
+        "The Addams Family (2019)"]
+  end
+
+  def get_user_selection
+    #list movies
+    @movies.each_with_index do |movie, index|
+        puts "#{index+1}. #{movie}"
+      end
+
+  end
 end
