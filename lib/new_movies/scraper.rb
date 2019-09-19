867156f054c30cc7a57e71dd41c37a02627c5149
Chris Cheong <chrisxcheong@gmail.com>
2019-09-19 00:18:08 +0000

    working on scraper class

--- lib/new_movies/scraper.rb
+++ lib/new_movies/scraper.rb
@@ -1,3 +1,22 @@
 class NewMovies::Scraper
+  require 'nokogiri'
+  require 'open-uri'
+  site = "https://www.fandango.com/"
+
+  doc = Nokogiri::HTML(open(site))
+
+  items = doc.css("li.media")
+
+  puts items.count
+
+  # articles = items.select{|item| item.css("figcaption.gallery-caption").count != 0}
+  # articles.count
+
+   items.each do |article|
+     title = article.css("a.heading-style-1").text
+     url = article.css("a")[0].attributes["href"]
+     puts url
+     puts title
+   end
 
 end
